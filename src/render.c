58a31fe240be0d17e6b81cfc932efb2b575eac36
lowlevelguy <virtual.code@protonmail.com>
2026-04-07 19:43:21 +0100

    Modified x_offset and bar_width computation in render_frame so that integer division rounding errors don't accumulate, and so that the last bar is glued to the right image border.

--- src/render.c
+++ src/render.c
@@ -15,12 +15,11 @@ void render_frame(double* levels, size_t levels_sz,
 	// and a third of its height; occupying the middle third
 	// top-down.
 	size_t bar_max_height = frame_height/3,
-		   bar_width = (size_t)(frame_width / (levels_sz + (levels_sz-1) / 4.0)),
-		   gap_width = bar_width/4,
-		   bar_height, x_offset;
+		   bar_width, bar_height, x_offset;
 	for (int i = 0; i < levels_sz; i++) {
 		bar_height = (CLAMP_DB(levels[i])-MIN_DB) * bar_max_height / (MAX_DB-MIN_DB);
-		x_offset = (bar_width + gap_width) * i;
+		x_offset = (frame_width * i * 5) / (levels_sz * 5 - 1);
+		bar_width = (frame_width * (i * 5 + 4)) / (levels_sz * 5 - 1) - x_offset;
 
 		for (int j = 0; j < bar_height; j++)
 			memset(&frame[(frame_height * 2/3 - 1 - j) * frame_width + x_offset], 255, bar_width * sizeof(pixel_t));
